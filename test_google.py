007c0b8cc8877252a2266a00daa4b2976bb3d1cf
ido21433 <164926692+ido21433@users.noreply.github.com>
2024-03-25 20:39:22 +0200

    Add files via upload

--- test_google.py
+++ test_google.py
@@ -1,18 +1,24 @@
-# selenium 4
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def chrome():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
+    return driver
+
 
 def test_title():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    
-    driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install(), options=options)
-    )
-
-    driver.get("https://www.google.com")
-    assert driver.title == "Google"
-    driver.quit()
+    driver = chrome()
+    driver.get("https://google.com")
+    assert "Google" in driver.title
+    driver.close()
