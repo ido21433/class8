f3f0506591a22bd82a356c2cc82540175bd8d1ee
ido21433 <164926692+ido21433@users.noreply.github.com>
2024-03-25 20:33:19 +0200

    Add files via upload

--- test_google.py
+++ test_google.py
@@ -2,11 +2,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
 
 def test_title():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    
     driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install())
+        ChromeDriverManager().install(), options=options)
     )
 
     driver.get("https://www.google.com")
